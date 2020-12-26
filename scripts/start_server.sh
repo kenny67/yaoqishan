5b921d5ebe484c4b89f9f4eb478c1feca7646d2e
kennyshen <282716545@qq.com>
2020-12-26 22:53:13 +0900

    appspec
    appspec

---
+++ scripts/start_server.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd /home/ec2-user/work/yaoqishan
+
+#sudo rm -f ./nohup.out
+
+#export SPRING_REDIS_HOST="redis-for-test.vkjgaw.0001.apne1.cache.amazonaws.com"
+
+#export SPRING_DATASOURCE_URL="jdbc:postgresql://database-6.cjrkfow6klcg.ap-northeast-1.rds.amazonaws.com:5432/weget_new_db"
+#export SPRING_DATASOURCE_USERNAME="postgres"
+#export SPRING_DATASOURCE_PASSWORD="password"
+
+#nohup java -jar manageApiService-1.0.jar > /dev/null 2>&1 &
+#
---
+++ scripts/stop_server.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#pkill -U $USER -f "java .*-jar manage.*\.jar"
+#exit 0
+# kill -s 9 8001
+
